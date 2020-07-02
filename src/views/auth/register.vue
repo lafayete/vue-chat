0a4a720955f3c75bcce6f8a42868bb4fac9d962f
wushibao <wushibao@chengfayun.com>
2020-07-02 21:59:51 +0800

    init vue chat

---
+++ src/views/auth/register.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <img src="../../assets/logo.png" class="logo">
+    <h2>欢迎使用Vue-Chat</h2>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .logo {
+    
+        width: 120px;
+
+        height: 120px;
+
+    }
+
+
+</style>
