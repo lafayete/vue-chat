de12c28b6cc2735b2a56c23fa1aba89d3f3f0da1
wushibao <wushibao@chengfayun.com>
2020-07-13 21:31:32 +0800

    add login

--- client/src/views/auth/login.vue
+++ client/src/views/auth/login.vue
@@ -1,19 +1,108 @@
 <template>
-  <div class="hello">
-    <h2>login</h2>
+  <div class="wrapper">
+    <img src="../../assets/logo.png" class="vue_logo">
+    <h2>欢迎使用Vue-Chat</h2>
+
+    <div class="item">
+        <img src="../../assets/phone.png" class="icon"/>
+        <input v-model="phone" />
+    </div>
+
+    <div class="item">
+        <img src="../../assets/password.png" class="icon" />
+        <input v-model="password" />
+    </div>
+
+    <div class="login" @click="register">登陆</div>
+
+    <p>尚未注册，去注册</p>
+
   </div>
 </template>
 
 <script>
+
+import { Toast } from 'mint-ui';
+
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+        phone: '',
+        password: '',
+    }
+  },
+
+  mounted() {
+  },
+
+  methods: {
+
+    register() {
+
+
     }
   }
+
 }
 </script>
 
 <style scoped>
+
+
+    .wrapper {
+    
+        display: flex;
+
+        flex-direction:column;
+
+        align-items: center;
+    
+    }
+
+    .vue_logo {
+    
+        width: 120px;
+
+        height: 120px;
+
+    }
+
+    .icon {
+        width: 22px;
+
+        height: 22px;
+    
+        margin-right: 10px;
+    
+    }
+
+    .register {
+    
+        width: 250px;
+
+        height: 50px;
+
+        line-height: 50px;
+
+        color: #fff;
+
+        background: #3CB371;
+
+        margin-top: 50px;
+    
+    }
+
+    .item {
+
+      display: flex;
+
+      align-items: center;
+
+      margin-top: 10px;
+
+    }
+
+
 </style>
